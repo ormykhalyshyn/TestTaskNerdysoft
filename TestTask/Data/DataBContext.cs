b4be9b22919ae829defe1cbd39d3047154f4eb2f
morest261 <or.mykhalyshyn@gmail.com>
2021-12-06 13:15:26 +0200

    Adding db

---
+++ TestTask/Data/DataBContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TestTask.Models;
+
+namespace TestTask.Data
+{
+    public class DataBContext:DbContext
+    {
+        public DataBContext(DbContextOptions<DataBContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Announcement> Announcements { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Announcement>().ToTable("Announcement");
+        }
+    }
+}
---
+++ TestTask/Data/DbInitializer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TestTask.Models;
+
+namespace TestTask.Data
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(DataBContext context)
+        {
+            context.Database.EnsureCreated();
+
+            // Look for any students.
+            if (context.Announcements.Any())
+            {
+                return;   // DB has been seeded
+            }
+
+            var announcements = new Announcement[]
+            {
+            new Announcement{Name="Qwerty",Description="QwertyQwerty",DateofCreation=DateTime.Parse("2021-12-06")},
+            new Announcement{Name="Qwerty2",Description="QwertyQwerty2",DateofCreation=DateTime.Parse("2021-12-06")},
+            new Announcement{Name="Qwerty3",Description="QwertyQwerty3",DateofCreation=DateTime.Parse("2021-12-06")},
+            new Announcement{Name="Qwerty4",Description="QwertyQwerty4",DateofCreation=DateTime.Parse("2021-12-06")},
+            };
+            foreach (Announcement i in announcements)
+            {
+                context.Announcements.Add(i);
+            }
+            context.SaveChanges();
+        }
+    }
+}
